8bc298a94242e0ef9f7e26a95f87f8604e0f6301
João Paulo Nunes Soares <paulonsoares@yahoo.com.br>
2020-07-24 14:52:25 -0300

    #4 - Adding enzyme as framework for coverage report integrated with  jest.

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,11 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
+
+//Command to run the tests: npm run test -- --coverage --watchAll=false
+
 import '@testing-library/jest-dom/extend-expect';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import 'jest-enzyme';
+configure({ adapter: new Adapter() });
