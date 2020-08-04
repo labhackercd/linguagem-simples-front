f88f3f220c6233e91aa33f7e0f2fbd29862c6d8d
teogenesmoura <mourateogenes@gmail.com>
2020-08-04 16:15:19 -0300

    #17 Adding menu on external content box of the dashboard

--- src/components/Dashboard/externalContentPanel.js
+++ src/components/Dashboard/externalContentPanel.js
@@ -1,17 +1,113 @@
 import React from 'react';
-import { Grid, Typography } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import { Grid, Typography, Button } from '@material-ui/core';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import SaveIcon from '@material-ui/icons/Save';
 
 
 const useStyles = makeStyles((theme) => ({
   body: {
-    padding: '0 1.5rem 0 1.5rem',
+    padding: '1rem',
+    height: '100%',
+    overflow: 'auto',
   },
+  menu: {
+    padding: '0.1rem',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+  },
+  button: {
+    margin: '0.1rem 0',
+    width: '100%',
+    height: '100% !important',
+    color: 'white',
+    '&:focus, &.Mui-focusVisible': {
+      backgroundColor: '#007E5A',
+      color: "white",
+    },
+  },
+  content: {
+    padding: '1rem',
+  }
 }));
+
+const MenuButton = withStyles((theme) => ({
+  root: {
+    color: "#666666",
+    backgroundColor: "#F9F9F9",
+    textTransform: 'none',
+    '&:focus': {
+      backgroundColor: '#007E5A',
+      color: "white",
+    },
+  },
+}))(Button);
 
 export default function ExternalContentPanel() {
   const classes = useStyles();
   return (
-    <h1> oh yeah </h1>
+    <Grid container className={classes.body}>
+      <Grid item md={4} className={classes.menu}>
+        <MenuButton
+          variant="contained"
+          size="large"
+          className={classes.button}
+          startIcon={<img src="../../img/flag_icon.svg"/>}
+          disableElevation
+          >
+          <Typography variant="body1">Conteúdos salvos</Typography>
+        </MenuButton>
+        <MenuButton
+          variant="contained"
+          size="large"
+          className={classes.button}
+          startIcon={<img src="../../img/paper_sheet_icon.svg"/>}
+          disableElevation
+          >
+          <Typography variant="body1">Agência Câmara</Typography>
+        </MenuButton>
+        <MenuButton
+          variant="contained"
+          color="default"
+          size="large"
+          className={classes.button}
+          startIcon={<SaveIcon/>}
+          disableElevation
+          >
+          <Typography variant="body1"> Tv Câmara </Typography>
+        </MenuButton>
+        <MenuButton
+          variant="contained"
+          color="default"
+          size="large"
+          className={classes.button}
+          startIcon={<img src="../../img/radio_icon.svg"/>}
+          disableElevation
+          >
+          <Typography variant="body1">Rádio Câmara</Typography>
+        </MenuButton>
+        <MenuButton
+          variant="contained"
+          color="default"
+          size="large"
+          className={classes.button}
+          startIcon={<img src="../../img/twitter_icon.svg"/>}
+          disableElevation
+          >
+          <Typography variant="body1">Twitter</Typography>
+        </MenuButton>
+        <MenuButton
+          variant="contained"
+          size="large"
+          className={classes.button}
+          startIcon={<img src="../../img/glossary_icon.svg"/>}
+          disableElevation
+          >
+          <Typography variant="body1">Glossário</Typography>
+        </MenuButton>
+      </Grid>
+      <Grid item md={8} className={classes.content}>
+      </Grid>
+    </Grid>
   )
 }
