b9480be31cb3138fb81f9ada38881715ea4c6dab
João Paulo Nunes Soares <paulonsoares@yahoo.com.br>
2020-08-18 10:32:48 -0300

    US12-AssistirSessaoTempoReal (#56)
    * #49 - Adding iframe youtube player function
    
    * #49 - Adding conditional rendering of transmission
    
    * #49 - Adding tests to render youtube

--- src/components/Dashboard/Content/index.test.js
+++ src/components/Dashboard/Content/index.test.js
@@ -2,8 +2,24 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Content from './index';
 import {shallow} from "enzyme/build";
+import Youtube from './youtubeTransmission'
+import ExternalContentPanel from './externalContentPanel'
 
 it("should render the content section", () => {
+    const component = shallow(<ExternalContentPanel/>);
+    expect(component).toMatchSnapshot();
+});
+
+it("should render the external content section", () => {
     const component = shallow(<Content/>);
     expect(component).toMatchSnapshot();
 });
+
+
+it("should return iframe of youtube content section", () => {
+    const videoID = "YUNat3PN8n8";
+    const youtubeVideo = Youtube(videoID);
+    expect(youtubeVideo.props.className).toBe("video")
+});
+
+
