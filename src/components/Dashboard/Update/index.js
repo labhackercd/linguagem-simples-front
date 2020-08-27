f99bbf160dbd0454dc60066369b563f5579a5a27
teogenesmoura <mourateogenes@gmail.com>
2020-08-27 16:38:08 -0300

    #62 Implementing restraint constraints before posting to API

--- src/components/Dashboard/Update/index.js
+++ src/components/Dashboard/Update/index.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {Paper,Button, TextField, Dialog, DialogActions, DialogContent, DialogContentText,
+import {Paper,Button, ButtonGroup, TextField, Dialog, DialogActions, DialogContent, DialogContentText,
 	      DialogTitle, Grid, Typography, Box,  List, ListItem, Tabs, Tab} from '@material-ui/core';
 import AppBar from '@material-ui/core/AppBar';
 import {makeStyles} from '@material-ui/core/styles';
@@ -108,15 +108,31 @@ const useStyles = makeStyles((theme) => ({
 		margin: '10px 0',
 	},
 	tabs: {
+		display: 'flex',
 		minHeight: '0px',
+		margin: '0.5rem 0 0 0',
+		overflow: 'auto',
+		flexWrap: 'no-wrap',
 	},
 	tab: {
-		padding: '0px',
-		margin: '0 0.1rem',
+		whiteSpace: 'nowrap',
+		padding: '0 0.5rem',
+		margin: '0 0.3rem',
 		backgroundColor: '#F2F2F2',
 		borderRadius: '5px',
 		minHeight: '0px',
+		minWidth: 'auto',
 		textTransform: 'capitalize'
+	},
+	sessionTitleAlert: {
+		backgroundColor: '#00AF82',
+		width: '100%',
+		borderRadius: '5px',
+		color: 'white',
+		padding: '0.2rem',
+		fontWeight: '600',
+		fontSize: '13',
+		display: 'flex',
 	}
 }));
 
@@ -131,55 +147,65 @@ export default function Update(props){
 	const [open, setOpen] = useState(false);
 	const [previewModalOpen, setPreviewModalOpen] = useState(false);
 	const [imageUploadModalOpen, setImageUploadModalOpen] = useState(false);
-	const [picture, setPicture] = useState([]);
+	const [picture, setPicture] = useState(false);
 	const [value, setValue] = React.useState(0);
-	const [postStatus, setPostStatus] = useState('');
-	const [titlesArray] = useState(['Pré-sessão',
+	const [titlesArray] = useState([
+																	'Pré-sessão',
 																	'Sessão Iniciada',
 																	'Votação Iniciada',
 																	'Votação Encerrada',
 																	'Próxima Pauta',
 																	'Sessão Encerrada',
 																	'Fim da transmissão'])
-
+	const errorMessages = {
+				lacks_payload_content: 'Voce deve submeter pelo menos uma atualizacao, ou imagem ou tweet para prosseguir'
+	}
 	function dispatchPayload() {
-		console.log('updateTitle: ' + updateTitle)
-		const formData = new FormData()
-		if(!(picture instanceof Array)) {
-			formData.append('image', picture, picture.name)
+		if (validatePayload()) {
+			const formData = new FormData()
+			if(picture) {
+				formData.append('image', picture, picture.name)
+			}
+			formData.append('title', updateTitle)
+			formData.append('content', updateTextArea)
+			formData.append('session', 1)
+			formData.append('tweet_id', tweetID)
+			axiosInstance.post('/publications/', formData, {
+				headers: { 'Content-Type': 'multipart/form-data'},
+			}).then(result => {
+				if(result.status===201) {
+						let data = result.data
+						console.log(data)
+						let newUpdate = {
+							id: data.id,
+							content: data.content,
+							time: parseHourMinute(new Date(data.created))
+						}
+						if(data.tweet_id.length > 0) {
+							newUpdate['tweet_id'] = data.tweet_id
+						}
+						if(data.image){
+							newUpdate['image'] = data.image
+						}
+						if(data.title) {
+							newUpdate['title'] = data.title
+						}
+						setUpdates([...updates, newUpdate])
+						garbageCollection()
+				}
+			}).catch(err => {
+				console.log(err)
+			})
+		} else {
+			alert(errorMessages.lacks_payload_content)
 		}
-		formData.append('title', updateTitle)
-		formData.append('content', updateTextArea)
-		formData.append('session', 1)
-		formData.append('tweet_id', tweetID)
-		axiosInstance.post('/publications/', formData, {
-			headers: { 'Content-Type': 'multipart/form-data'},
-		}).then(result => {
-			if(result.status===201) {
-					let data = result.data
-					console.log(data)
-					let newUpdate = {
-						id: data.id,
-						content: data.content,
-						time: parseHourMinute(new Date(data.created))
-					}
-					if(data.tweet_id.length > 0) {
-						newUpdate['tweet_id'] = data.tweet_id
-					}
-					if(data.image){
-						newUpdate['image'] = data.image
-					}
-					if(data.title) {
-						newUpdate['title'] = data.title
-					}
-					setUpdates([...updates, newUpdate])
-			}
-			if (result.status===400) {
-				console.log(result)
-			}
-		}).catch(err => {
-			console.log(err)
-		})
+	}
+	{/* At least one field among content, image and tweet must not be null
+	 before we send a request to the API */}
+	function validatePayload() {
+		let contentExists = updateTextArea.length > 0
+		let tweetExists = tweetID.length > 0
+		return picture || contentExists || tweetExists;
 	}
 	function parseHourMinute(date) {
 		return date.getHours() + ':' + ('0'+ date.getMinutes()).slice(-2)
@@ -201,36 +227,34 @@ export default function Update(props){
 	}
 	function garbageCollection() {
 		setTweetID('')
-		setPicture([])
+		setPicture(false)
+		setUpdateTitle('')
+		setUpdateTextArea('')
 	}
 	function handleImageUploadDialogOpen(e){
 		e.preventDefault()
 		setImageUploadModalOpen(true)
 	}
 	function handleImageUploadDialogClose() {
-		if(picture instanceof Array) {
-			alert("Imagem nao foi carregada")
-		} else {
 			setImageUploadModalOpen(false)
 			dispatchPayload()
-			garbageCollection()
-		}
 	}
 	function handlePreviewModalClose() {
 		dispatchPayload()
 		setPreviewModalOpen(false)
-		garbageCollection()
 	}
 	function handleChange(e) {
 		setUpdateTextArea(e.target.value)
 	}
 	function onImageDrop(picture) {
-		setPicture(picture[0])
+		picture.length > 0 ? setPicture(picture[0]) : setPicture(false)
 	}
-	function handleUpdateTitle(title) {
-		updateTitle.length > 0 ?
-		setUpdateTitle("") :
-		setUpdateTitle(title)
+	function startUpdateWithTitleFlow(e, title) {
+		handleSessionTitle(title)
+		handleImageUploadDialogOpen(e)
+	}
+	function handleSessionTitle(title) {
+		title.length > 0 ? setUpdateTitle(title) : setUpdateTitle('')
 	}
 	function a11yProps(index) {
 	 return {
@@ -241,6 +265,7 @@ export default function Update(props){
 	return (
 		<React.Fragment>
 			<div className={classes.body}>
+
 			{/* Summary box */}
 			<Grid container className={classes.summaryBox}>
 				<Grid container className={classes.summaryHeader}>
@@ -281,6 +306,7 @@ export default function Update(props){
 						</Grid>
 					</Box>
 			</Grid>
+			{/* End of Summary Box */}
 
 			{/* Status selection component*/}
 			<Grid container className={classes.summaryBox}>
@@ -291,20 +317,11 @@ export default function Update(props){
 					<Grid item md={6} style={{display:'flex', justifyContent: 'flex-end'}}>
 					</Grid>
 				</Grid>
-	        <Tabs
-	          value={value}
-	          variant="scrollable"
-	          scrollButtons="on"
-						TabScrollButtonProps={{disabled: false}}
-						classes={{root: classes.tabs}}
-						indicatorColor="white"
-						aria-label="scrollable auto tabs"
-						>
-
+				<Grid className={classes.tabs}>
 						{titlesArray.map(function(title, index){
-							return <Tab className={classes.tab} label={title} onClick={() => handleUpdateTitle(title)} {...a11yProps(index)} />
-						})}
-	        </Tabs>
+						return <Button variant="contained" onClick={(e) => startUpdateWithTitleFlow(e, title)} disableElevation className={classes.tab}>{title}</Button>
+					})}
+				</Grid>
 			</Grid>
 			{/* End of status selection component */}
 
@@ -363,7 +380,6 @@ export default function Update(props){
 							</Box>
 					</Grid>
 				</Grid>
-
 				{/* Twitter URL input dialog */}
 					<Dialog open={open} onClose={handleTwitterDialogClose}>
 					 <DialogContent>
@@ -392,6 +408,7 @@ export default function Update(props){
 				 </Dialog>
 				{/* End of Twitter URL input dialog */}
 
+
 				{/* Image Upload Dialog */}
 				<Dialog fullWidth={true}
 									maxWidth={'sm'}
@@ -408,9 +425,17 @@ export default function Update(props){
 					 </div>
 					 <Paper style={{backgroundColor: 'white', padding: '1rem', borderRadius: '15px'}} elevation={0}>
 				 		<DialogContent>
+							<Grid style={{display: updateTitle ? 'flex' : 'none' }}
+										container
+										className={classes.sessionTitleAlert}>
+								<Grid item md={1} style={{padding: '0.1rem 0 0 0.5rem'}}><img src="../../img/alert.svg"/></Grid>
+								<Grid item md={10}>{updateTitle}</Grid>
+								<Grid item md={1} style={{padding: '0.1rem 0.1rem 0rem 1rem'}}><img src="../../img/alert_within_dialog_exit_icon.svg" onClick={() => setUpdateTitle('')} /></Grid>
+							</Grid>
 						 <TextField
 							 id="textfield"
 							 multiline
+							 fullWidth={true}
 							 rows={4}
 							 bgcolor="white"
 							 onChange={(e) => setUpdateTextArea(e.target.value)}
@@ -437,12 +462,12 @@ export default function Update(props){
 							/>
 					 </DialogContent>
 				 </Paper>
-					 <DialogActions className={classes.previewModalFooter}>
-						 <Typography className={classes.time} style={{alignSelf: 'flex-start'}} variant="h6"> 18:00 </Typography>
-						 <Button onClick={handleImageUploadDialogClose} className={classes.previewModalSubmitButton} variant="contained">
-							 Fazer upload da imagem
-						 </Button>
-					 </DialogActions>
+				 <DialogActions className={classes.previewModalFooter}>
+					 <Typography className={classes.time} style={{alignSelf: 'flex-start'}} variant="h6"> 18:00 </Typography>
+					 <Button onClick={handleImageUploadDialogClose} className={classes.previewModalSubmitButton} variant="contained">
+						 Publicar
+					 </Button>
+				 </DialogActions>
 				 </Dialog>
 				{/* End of Image Upload Dialog */}
 
