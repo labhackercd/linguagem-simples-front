7cfba6733f2971bf2734a49bf52469f4bd8796c9
João Paulo Nunes Soares <paulonsoares@yahoo.com.br>
2020-09-08 12:11:40 -0300

    #73- Refactoring videosnippetmodal and adding tests (#82)

--- src/components/Dashboard/Content/FetchFunctions/postSaveContent.test.js
+++ src/components/Dashboard/Content/FetchFunctions/postSaveContent.test.js
@@ -52,6 +52,28 @@ describe('Test postSaveContent function requisitions with mock adapter', () => {
         done()
     });
 
+    test("Test if postSaveContent returns false when it has not worked correct and status != 201", async (done) => {
+        const dashboarId = 1;
+        var response = null;
+        const content_type = "news";
+        const content_info = {
+            title:"Title",
+            url:"www.teste.com.br"
+        }
+        
+        mock.onPost("/saved-contents/").replyOnce(202,{
+            "url": [
+                "Entrar um URL válido."
+              ]
+        });
+
+        response = await postSaveContent(content_type,content_info,dashboarId);
+        expect(response).not.toBeNull();
+        expect(response).not.toBeUndefined();
+        expect(response).not.toBeTruthy()
+        done()
+    });
+
     afterAll(() => {
         mock.restore();
     });
