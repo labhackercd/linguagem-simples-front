4a73383d5aef5f619db32f8e360fdced571d4dc7
teo <teo@teos-MacBook-Pro.local>
2020-09-23 12:05:46 -0300

    #94 Updating style to broadcast ending dialog

--- src/components/Dashboard/Timeline/Dialogs/Alert/Broadcast/endBroadcast.js
+++ src/components/Dashboard/Timeline/Dialogs/Alert/Broadcast/endBroadcast.js
@@ -1,9 +1,43 @@
 import React, {useState, useEffect} from 'react';
 import {Paper,Button, TextField, Dialog, DialogActions, DialogContent, DialogContentText,
-	      DialogTitle, Grid, Typography, Box,  List, ListItem} from '@material-ui/core';
+	      DialogTitle, Grid, Typography, Box, IconButton, List, ListItem} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import BroadcastOffIcon from './../../../../../../assets/broadcast_off.svg';
+import ExitIcon from './../../../../../../assets/exit_icon.svg';
+
+const useStyles = makeStyles((theme) => ({
+	dialogPaper: {
+    maxWidth: '25%',
+	},
+  dialogContextTest: {
+    fontSize: theme.typography.h4.fontSize,
+		fontStyle: theme.typography.h4.fontStyle,
+		color: '#000',
+  },
+	broadcastIcon: {
+		display: 'flex'
+	},
+	exitIcon: {
+	},
+	cancelButton: {
+		backgroundColor: theme.palette.cinza1.main,
+		padding: '0.1rem 0.5rem'
+	},
+	OkButton: {
+		backgroundColor: theme.palette.verdeCamaraLight.main,
+		color: theme.palette.white.main,
+		padding: '0.1rem 0.5rem'
+	},
+  friendlyWarning: {
+    padding: '0 1rem 0 0',
+    fontSize: theme.typography.body1.fontSize,
+    fontStyle: theme.typography.body1.fontStyle,
+    color: theme.palette.grey.main,
+  }
+}))
 
 export default function EndBroadcastAlert(props) {
-
+  const classes = useStyles();
 	const { onClose, value: valueProp, action, open, ...other } = props;
 	const [value, setValue] = React.useState(valueProp);
 
@@ -22,20 +56,42 @@ export default function EndBroadcastAlert(props) {
 	}
 
   return (
-		<>
-			<Dialog open={open}>
-			 <DialogContent>
-				 <DialogContentText>
-					 Confirmar fim da transmissão?
-				 </DialogContentText>
-			 </DialogContent>
+    <>
+			<Dialog open={open} PaperProps={{ classes: {root: classes.dialogPaper } }}>
+			 	<Grid container>
+					<Grid item sm={11}></Grid>
+					<Grid item sm={1}>
+					<IconButton aria-label="exit">
+						<img src={ExitIcon} />
+					</IconButton>
+					</Grid>
+				</Grid>
+			 	<Grid container>
+					<Grid item sm={1}></Grid>
+					<Grid item
+								alignItems="center"
+	  						justify="center"
+								sm={3}
+								className={classes.broadcastIcon}>
+						<img src={BroadcastOffIcon} alt="broadcast"/>
+					</Grid>
+					<Grid item sm={1}></Grid>
+					<Grid item sm={7}>
+						<DialogContentText className={classes.dialogContextTest}>
+							Confirmar fim da transmissão?
+						</DialogContentText>
+            <DialogContentText className={classes.friendlyWarning}>
+              Certifique-se de inserir um resumo da sessão antes de encerrar.
+              Após encerrada, o resumo não poderá ser editado.
+            </DialogContentText>
+					</Grid>
+				</Grid>
 			 <DialogActions>
-				 <Button onClick={handleCancel}>
+				 <Button className={classes.cancelButton} variant="contained" disableElevation onClick={handleCancel}>
 					 Cancelar
 				 </Button>
-				 <Button onClick={handleOk}
-								 color="primary">
-					 Ok
+				 <Button className={classes.OkButton} variant="contained" disableElevation onClick={handleOk}>
+					 Sim
 				 </Button>
 			 </DialogActions>
 		 </Dialog>
