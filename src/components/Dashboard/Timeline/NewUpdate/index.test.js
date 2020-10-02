43d6c7dc94656ed84e0bc71404e944a9fe40dc77
teo <teo@teos-MacBook-Pro.local>
2020-10-02 16:38:00 -0300

    #110 Updating snapshot

--- src/components/Dashboard/Timeline/NewUpdate/index.test.js
+++ src/components/Dashboard/Timeline/NewUpdate/index.test.js
@@ -7,7 +7,7 @@ import ReactDOM from 'react-dom';
 import MockTheme from './../mockTheme';
 
 it("snapshot should not have differences", () => {
-    const component = shallow(<MockTheme><NewUpdate date={new Date(1995,11,17,3,24,0)}/></MockTheme>);
+    const component = shallow(<MockTheme><NewUpdate date={"December 17, 1995 03:24:00"}/></MockTheme>);
     expect(component.exists()).toEqual(true);
     expect(component).toMatchSnapshot();
 });
