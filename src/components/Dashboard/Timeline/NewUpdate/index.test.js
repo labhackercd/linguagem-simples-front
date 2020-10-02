a1f56ff46a9183dd26cb8615f1a1467a03948dc3
teo <teo@teos-MacBook-Pro.local>
2020-10-02 16:01:32 -0300

    #110 Fixing snapshot tests

--- src/components/Dashboard/Timeline/NewUpdate/index.test.js
+++ src/components/Dashboard/Timeline/NewUpdate/index.test.js
@@ -4,21 +4,24 @@ import userEvent from '@testing-library/user-event';
 import NewUpdate from './index';
 import {shallow, mount } from "enzyme/build";
 import ReactDOM from 'react-dom';
+import MockTheme from './../mockTheme';
 
 it("snapshot should not have differences", () => {
-    const component = shallow(<NewUpdate date={new Date(98, 1)}/>);
+    const component = shallow(<MockTheme><NewUpdate date={new Date(98, 1)}/></MockTheme>);
     expect(component.exists()).toEqual(true);
     expect(component).toMatchSnapshot();
 });
 
 test('Test if NewUpdate renders without crash', () => {
     const div = document.createElement("div")
-    ReactDOM.render(<NewUpdate></NewUpdate>, div)
+    ReactDOM.render(<MockTheme><NewUpdate></NewUpdate></MockTheme>, div)
     ReactDOM.unmountComponentAtNode(div)
 });
 
 test('writes update and submit doesnt break page', () => {
-    const wrapper = mount(<NewUpdate handleDialogStateAction={jest.fn()}/>);
+    const wrapper = mount(<MockTheme>
+                          <NewUpdate handleDialogStateAction={jest.fn()}/>
+                          </MockTheme>);
     const button = wrapper.find("#updateSubmitButton").last();
     const textField = wrapper.find("#newUpdateTextField").last();
     textField.instance().value = "test content"
@@ -28,7 +31,7 @@ test('writes update and submit doesnt break page', () => {
 });
 
 test('clicking content insertion icons doesnt break page', () => {
-  const wrapper = mount(<NewUpdate handleDialogStateAction={jest.fn()}/>);
+  const wrapper = mount(<MockTheme><NewUpdate handleDialogStateAction={jest.fn()}/></MockTheme>);
   const imageInsert = wrapper.find('#picture-upload-icon').last()
   const twitterInsert = wrapper.find('#tweet-insert-icon').last()
   const linkInsert = wrapper.find('#link-insert-icon').last()
--- src/components/Dashboard/Timeline/SummaryBox/index.test.js
+++ src/components/Dashboard/Timeline/SummaryBox/index.test.js
@@ -3,27 +3,28 @@ import { render, screen } from '@testing-library/react';
 import SummaryBox from './index';
 import {shallow, mount} from "enzyme/build";
 import ReactDOM from 'react-dom';
+import MockTheme from './../mockTheme';
 
 it("snapshot should not have differences", () => {
-    const component = shallow(<SummaryBox/>);
+    const component = shallow(<MockTheme><SummaryBox/></MockTheme>);
     expect(component).toMatchSnapshot();
 });
 
 test('Test if SummaryBox renders without crash', () => {
     const div = document.createElement("div")
-    ReactDOM.render(<SummaryBox></SummaryBox>, div)
+    ReactDOM.render(<MockTheme><SummaryBox></SummaryBox></MockTheme>, div)
     ReactDOM.unmountComponentAtNode(div)
 });
 
 
 test('does "resumo" label gets rendered', () => {
-    const { getByText } = render(<SummaryBox />);
+    const { getByText } = render(<MockTheme><SummaryBox /></MockTheme>);
     const textAreaLabel = getByText('Resumo');
     expect(textAreaLabel).toBeInTheDocument();
 });
 
 test('click on update button doesnt break page', () => {
-  const wrapper = mount(<SummaryBox sessionId={1} broadcastingOnline={true} />);
+  const wrapper = mount(<MockTheme><SummaryBox sessionId={1} broadcastingOnline={true} /></MockTheme>);
   const submitButton = wrapper.find('#summary-box-submit-button').last()
   const textfield = wrapper.find('#summary-box-textfield').last()
   textfield.instance().value = "test content"
